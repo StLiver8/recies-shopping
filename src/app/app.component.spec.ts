7789083a298bae5f6d4d73eb8be64da6b1e1456e
Stefan Krasnov <SKrasnov21@gmail.com>
2020-12-08 13:24:26 +0300

    'First'

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('Component: App', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    });
+  });
+
+  // it('should create the app', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app).toBeTruthy();
+  // });
+});
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyDtX2ru6FaMF_zzMbUoR2nslqPBJOOvI4I'
+};
